3bd81c242e6602c6f88456e06c364b13848b8988
Juan Diego Trujillo <jd.trujillom@uniandes.edu.co>
2020-11-10 19:43:07 -0500

    Retos finalizados

--- src/components/job.js
+++ src/components/job.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import {FormattedDate, FormattedNumber, FormattedPlural, FormattedMessage} from 'react-intl';
 export default class Job extends React.Component {
+
+
 
   	render() {
   		return (
@@ -8,9 +10,17 @@ export default class Job extends React.Component {
   				<th scope="row">{this.props.offer.id}</th>
       			<td>{this.props.offer.name}</td>
   				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
+      			<td>{this.props.offer.salary}<FormattedPlural value={this.props.offer.salary} one=" million" other=" millions" /></td>
       			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
+      			<td><FormattedDate
+    value={new Date(this.props.offer.date)}
+    year='numeric'
+    month='long'
+    day='numeric'
+    weekday='long'
+  /></td>
+  				<td><FormattedNumber value={this.props.offer.views}/>
+				  </td>
   			</tr>
   		);
 	}
